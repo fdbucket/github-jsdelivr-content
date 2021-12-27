4cf60a5d6e48335245dcd2805db4c511b04c50cc
Jackson Dou <doudoudzj@qq.com>
2021-12-27 22:46:07 +0800

    update

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 // 配置文件
-define("TYPE", "GITHUB"); // 选择GITHUB/GITEE, 选择gitee，如果使用gitee，需要手动建立master分支，可以看这里 https://gitee.com/help/articles/4122
-define("USER", "pic-cdn"); // 你的GitHub/Gitee的用户名
-define("REPO", "cdn2"); // 必须是上面用户名下的 公开仓库
-define("NAME", "游客"); // 提交者显示名字
-define("EMAIL", "user@foxmail.com"); // 提交者邮箱地址，可以是账号绑定邮箱亦可以任意邮箱
-define("TOKEN", "d0d16844-dd7f-49ab-8765-79dc400564b8");
+define('TYPE', 'GITHUB'); // 选择GITHUB/GITEE, 选择gitee，如果使用gitee，需要手动建立master分支，可以看这里 https://gitee.com/help/articles/4122
+define('USER', 'pic-cdn'); // 你的GitHub/Gitee的用户名
+define('REPO', 'cdn2'); // 必须是上面用户名下的 公开仓库
+define('NAME', '游客'); // 提交者显示名字
+define('EMAIL', 'user@foxmail.com'); // 提交者邮箱地址，可以是账号绑定邮箱亦可以任意邮箱
+define('TOKEN', 'd0d16844-dd7f-49ab-8765-79dc400564b8');
 // Github 去这个页面 https://github.com/settings/tokens生成一个有写权限的token（repo：Full control of private repositories 和write:packages前打勾）
 // Gitee 去往这个页面 https://gitee.com/personal_access_tokens
 
